09d33308fe6a1b92545f6e9597a7f56b2fb5c2f1
Pavel Prischepa <pprischepa@gmail.com>
2016-05-09 15:31:59 +0600

    Updated

--- rootfs/etc/cont-init.d/30-init-nginx.sh
+++ rootfs/etc/cont-init.d/30-init-nginx.sh
@@ -1,10 +1,8 @@
-#!/bin/sh
-
-with-contenv
+#!/usr/bin/with-contenv sh
 
 if [ ! -d $WODBY_HOME/conf/nginx ] && [ ! -L /etc/nginx ]; then
     mv /etc/nginx $WODBY_CONF/
-    chown -R WODBY_USER:WODBY_GROUP $WODBY_HOME/conf/nginx
+    chown -R $WODBY_USER:$WODBY_GROUP $WODBY_HOME/conf/nginx
     ln -nsf $WODBY_HOME/conf/nginx /etc/nginx
 
 elif [ -d $WODBY_HOME/conf/nginx ] && [ ! -L /etc/nginx ]; then
